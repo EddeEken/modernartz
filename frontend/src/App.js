ef3b4350b9af04fb687de0e9e8e56aa2b52096b9
Edvin Ekström <ekstrom.edvin@gmail.com>
2023-12-13 11:17:15 +0100

    Fix bugg?

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Marketplace from "./components/Temporary";
+
+function App() {
+  return (
+    <div className="NTF-Marketplace">
+      <Marketplace></Marketplace>
+    </div>
+  );
+}
+
+export default App;
